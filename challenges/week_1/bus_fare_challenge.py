189ffd2147d758bbab5ce547f7220cac357f633c
ikim <60238859+ikimathi@users.noreply.github.com>
2021-02-02 05:47:49 +0300

    bus_fare_challenge complete!

--- challenges/week_1/bus_fare_challenge.py
+++ challenges/week_1/bus_fare_challenge.py
@@ -1,1 +1,22 @@
 # WRITE YOUR CODE SOLUTION HERE
+import datetime
+
+date = datetime.date.today()
+print("Date:", date)
+
+day = date.weekday()
+day_list = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sunday"]
+
+dayname = day_list[day]
+
+print("Day: ", dayname)
+
+
+if day <= 4:
+    print("Fare: 100")
+elif day == 5:
+    print("Fare: 60")
+elif day == 6:
+    print("Fare: 80")
+else:
+    print("Something is wrong!")
